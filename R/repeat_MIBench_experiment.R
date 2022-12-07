dbcfad83c4991a0e1a41d0fd46f10936208e50ce
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2022-12-07 14:10:44 -0500

    added seed management to repeat_MIBench_experiment

--- R/repeat_MIBench_experiment.R
+++ R/repeat_MIBench_experiment.R
@@ -6,6 +6,7 @@
 #' @param store_runs Set to TRUE if you want to store the output of the experiments. Default is FALSE.
 #' @param n_repetitions The number of repetitions (default is 1000) with fresh draws from the dgp function.
 #' @param n_cores The number of cores for parallel processing of the experiments.
+#' @param seed A random seed for the experiments. Note that a exact replication depends on the seed and the number of cores `n_cores`.
 #' @param ... Additional arguments for the dgp function
 #' @return A list of class "MIbench_imputations"
 #' @export
@@ -18,12 +19,30 @@
 #' # Explicitily specifying a scenario.
 #' simloglm(regression, scenario = list(speed = c(5, 10, 20)))
 
-repeat_MIbench_experiment <- function(dgp, MIalgorithm, m = 10, store_runs = TRUE, n_repetitions = 1000, n_cores = 4, ...){
-  bettermc::mclapply(
-    X = 1:n_repetitions,
-    FUN = function(x)
-      MIBench_experiment(dgp = dgp, MIalgorithm = MIalgorithm, m = m, suffix = x, store_runs = store_runs, ...),
-    mc.cores = n_cores,
-    mc.cleanup = TRUE
-  )
-}
+repeat_MIbench_experiment <-
+  function(dgp,
+           MIalgorithm,
+           m = 10,
+           store_runs = TRUE,
+           n_repetitions = 1000,
+           n_cores = 4,
+           seed = NULL,
+           ...) {
+    RNGkind("L'Ecuyer-CMRG")
+    set.seed(seed)
+
+    bettermc::mclapply(
+      X = 1:n_repetitions,
+      FUN = function(x)
+        MIBench_experiment(
+          dgp = dgp,
+          MIalgorithm = MIalgorithm,
+          m = m,
+          suffix = x,
+          store_runs = store_runs,
+          ...
+        ),
+      mc.cores = n_cores,
+      mc.cleanup = TRUE
+    )
+  }
