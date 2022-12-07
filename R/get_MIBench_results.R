47333745e9c3004250cc43f38442aef854e9e553
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2022-12-07 14:33:07 -0500

    added results summary

---
+++ R/get_MIBench_results.R
@@ -0,0 +1,71 @@
+#' Get the results from the output of repeat_MIBench_experiment
+#'
+#' @param obj The output of a run of `repeat_MIBench_experiment`
+#' @return A list of class "MIbench_imputations"
+#' @export
+#'
+#' @examples
+#' df <- cars
+#' regression <- lm(log(dist)~speed, data = df)
+#' # Specifiying no scenario to simulate at the mean of speed.
+#' simloglm(regression)
+#' # Explicitily specifying a scenario.
+#' simloglm(regression, scenario = list(speed = c(5, 10, 20)))
+#'
+get_MIBench_results <- function(obj) {
+  mi_uncongenial_combining_rules <- lapply(obj, function(x)
+    tryCatch(
+      MIBench:::analyze_mi(x$imputations, x$analysis_model, congenial = FALSE),
+      error = function(e)
+        NA
+    ))
+
+  mi_congenial_combining_rules <- lapply(obj, function(x)
+    tryCatch(
+      MIBench:::analyze_mi(x$imputations, x$analysis_model, congenial = TRUE),
+      error = function(e)
+        NA
+    ))
+
+  lwd <- lapply(obj, function(x) {
+    tmp <- x$analysis_model(x$D_mis)
+
+    res <- cbind(coef(tmp), confint(tmp))
+
+    colnames(res) <- c("estimate", "2.5 %", "97.5 %")
+    rownames(res) <- NULL
+    return(res)
+  })
+
+  infeasible <- lapply(obj, function(x) {
+    tmp <- x$analysis_model(x$D)
+
+    res <- cbind(coef(tmp), confint(tmp))
+
+    colnames(res) <- c("estimate", "2.5 %", "97.5 %")
+    rownames(res) <- NULL
+    return(res)
+  })
+
+
+  results_uncongenial <-
+    MIBench:::summarize_mi_analysis(Filter(Negate(anyNA), uncon_list), obj[[1]]$true_values)
+
+  results_congenial <-
+    MIBench:::summarize_mi_analysis(Filter(Negate(anyNA), con_list), obj[[1]]$true_values)
+
+  results_lwd <-
+    MIBench:::summarize_mi_analysis(Filter(Negate(anyNA), lwd), obj[[1]]$true_values)
+
+  results_infeasible <-
+    MIBench:::summarize_mi_analysis(Filter(Negate(anyNA), infeas), obj[[1]]$true_values)
+
+  res <- list(
+    congenial = results_congenial,
+    uncongenial = results_uncongenial,
+    lwd = results_lwd,
+    infeasible = results_infeasible
+  )
+
+  return(res)
+}
